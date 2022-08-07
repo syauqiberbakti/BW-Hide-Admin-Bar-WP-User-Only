c7be454535909f82d065a85aac6def982e8e3b89
Ahmad Syauqi <97066776+syauqiberbakti@users.noreply.github.com>
2022-08-07 15:12:50 +0700

    Add files via upload

---
+++ BW Hide Admin Bar WP (User Only).php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+
+Plugin Name: BW Hide Admin Bar WP (User Only)
+Plugin URI: https://www.berbakti.com/plugin-wordpress/bw-hide-admin-bar-wp
+Description: BW Hide Admin Bar WP is a plugin that has the ability to hide the appearance of the WordPress Admin Bar. The existence of this plugin makes it easy for WordPress users to hide the Admin Bar, whether all users or WP administrators are made an exception.
+Version: 1.0.0
+Author: Berbakti Webhoster
+Author URI: https://www.berbakti.com/
+Donation URI: https://www.berbakti.com/donation/
+Development URI: https://www.fajarpos.com/
+License: GPL v2.0
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+*/
+
+/*Hide Admin Bar Wordpress only User */
+
+add_action('after_setup_theme', 'remove_admin_bar');
+function remove_admin_bar() {
+if (!current_user_can('administrator') && !is_admin()) {
+  show_admin_bar(false);
+}
+}
+
+?>
